faf34db90ea62d91868430ebc24383ead428303e
Vusal <iamvusalalakbarov@gmail.com>
2025-01-03 16:22:21 +0400

    Add Prettier and ESLint configuration with Tailwind CSS support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
-import type { Metadata } from "next";
-import { Manrope } from "next/font/google";
-import "../styles/globals.css";
+import type { Metadata } from 'next';
+import { Manrope } from 'next/font/google';
+import NotificationBar from '@/components/NotificationBar';
+import '../styles/globals.css';
+import Header from '@/components/Header';
 
 const manrope = Manrope({
-  variable: "--font-manrope",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  variable: '--font-manrope',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
-  title: "Klothink",
-  description: "Developed by Vusal Alakbarov",
+  title: 'Klothink',
+  description: 'Developed by Vusal Alakbarov',
 };
 
 interface IRootLayoutProps {
@@ -21,6 +23,8 @@ export default function RootLayout(props: Readonly<IRootLayoutProps>) {
   return (
     <html lang="en">
       <body className={`${manrope.variable} antialiased`}>
+        <NotificationBar />
+        <Header />
         {props.children}
       </body>
     </html>
