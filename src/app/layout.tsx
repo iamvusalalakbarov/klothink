5ab481c9d21fda2f04cd372371c784d9cfbf6880
Vusal <iamvusalalakbarov@gmail.com>
2025-01-04 23:45:55 +0400

    Add MantineProvider and integrate Footer into RootLayout for improved styling and structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from 'next';
 import { Manrope } from 'next/font/google';
+import {
+  ColorSchemeScript,
+  MantineProvider,
+  mantineHtmlProps,
+} from '@mantine/core';
 import NotificationBar from '@/components/NotificationBar';
-import '../styles/globals.css';
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import '../styles/globals.css';
+import '@mantine/core/styles.css';
 
 const manrope = Manrope({
   variable: '--font-manrope',
@@ -21,11 +28,17 @@ interface IRootLayoutProps {
 
 export default function RootLayout(props: Readonly<IRootLayoutProps>) {
   return (
-    <html lang="en">
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+      </head>
       <body className={`${manrope.variable} antialiased`}>
-        <NotificationBar />
-        <Header />
-        {props.children}
+        <MantineProvider>
+          <NotificationBar />
+          <Header />
+          {props.children}
+          <Footer />
+        </MantineProvider>
       </body>
     </html>
   );
