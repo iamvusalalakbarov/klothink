b4ebcf663a01bf61be1c6a10933a8bc38e7a8c2d
Vusal <iamvusalalakbarov@gmail.com>
2025-01-04 23:45:41 +0400

    Add Footer component with navigation and subscription functionality

---
+++ src/components/Footer.tsx
@@ -0,0 +1,234 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import CustomButton from './ui/CustomButton';
+import { CustomButtonVariant } from '@/types/enums';
+import SocialNetworks from './SocialNetworks';
+
+const menu = [
+  {
+    title: 'Home',
+    link: '#',
+    sub: [
+      {
+        title: 'Features',
+        link: '#',
+      },
+      {
+        title: 'Popular Products',
+        link: '#',
+      },
+      {
+        title: 'Testimonials',
+        link: '#',
+      },
+      {
+        title: 'FAQ',
+        link: '#',
+      },
+    ],
+  },
+  {
+    title: 'Menswear',
+    link: '#',
+    sub: [
+      {
+        title: 'Casual',
+        link: '#',
+      },
+      {
+        title: 'Formal',
+        link: '#',
+      },
+      {
+        title: 'Party',
+        link: '#',
+      },
+      {
+        title: 'Business',
+        link: '#',
+      },
+    ],
+  },
+  {
+    title: 'Womenswear',
+    link: '#',
+    sub: [
+      {
+        title: 'Casual',
+        link: '#',
+      },
+      {
+        title: 'Formal',
+        link: '#',
+      },
+      {
+        title: 'Party',
+        link: '#',
+      },
+      {
+        title: 'Business',
+        link: '#',
+      },
+    ],
+  },
+  {
+    title: 'Kidswear',
+    link: '#',
+    sub: [
+      {
+        title: 'Casual',
+        link: '#',
+      },
+      {
+        title: 'Formal',
+        link: '#',
+      },
+      {
+        title: 'Party',
+        link: '#',
+      },
+    ],
+  },
+];
+
+const mobileMenu = [
+  {
+    title: 'Home',
+    link: '#',
+  },
+  {
+    title: 'Products',
+    link: '#',
+  },
+  {
+    title: 'Contact',
+    link: '#',
+  },
+];
+
+const copyrightLinks = [
+  {
+    title: 'Privacy Policy',
+    link: '#',
+  },
+  {
+    title: 'Terms & Conditions',
+    link: '#',
+  },
+];
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="border-y border-light-95 pt-[30px] laptop:pt-0">
+        <div className="wrapper laptop:flex">
+          <div className="space-y-[30px] laptop:p-20 laptop:pl-0">
+            <div className="space-y-6 laptop:min-w-[355px] laptop:space-y-[30px] desktop:space-y-[60px]">
+              <div className="flex items-center justify-between">
+                <Link
+                  href="/"
+                  className="relative size-10 laptop:size-12 desktop:size-20"
+                >
+                  <Image
+                    src="/images/logo.svg"
+                    alt="Klothink Logo"
+                    fill
+                    unoptimized
+                  />
+                </Link>
+
+                <div className="laptop:hidden">
+                  <SocialNetworks />
+                </div>
+              </div>
+
+              <form
+                method="post"
+                className="flex items-center gap-x-2 desktop:gap-x-2.5"
+              >
+                <input
+                  type="email"
+                  placeholder="Enter Your Email"
+                  className="w-full rounded-[32px] border border-light-95 bg-light-99 px-4 py-[14px] text-sm font-medium text-grey-15 outline-none placeholder:text-grey-60"
+                />
+
+                <CustomButton
+                  variant={CustomButtonVariant.PRIMARY}
+                  className="px-6"
+                >
+                  Subscribe
+                </CustomButton>
+              </form>
+            </div>
+
+            <ul className="flex items-center justify-center gap-x-6 border-t border-light-95 px-[60px] py-6 laptop:hidden">
+              {mobileMenu.map((item, index) => (
+                <li key={index}>
+                  <Link
+                    href={item.link}
+                    className="text-sm font-medium text-grey-15"
+                  >
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="hidden flex-1 items-start justify-between self-end border-l border-light-95 p-[60px] pr-0 laptop:flex">
+            {menu.map((item, index) => (
+              <div key={index} className="space-y-6">
+                <Link
+                  href={item.link}
+                  className="text-lg font-medium text-grey-15"
+                >
+                  {item.title}
+                </Link>
+
+                <ul className="space-y-[14px]">
+                  {item.sub.map((subItem, subIndex) => (
+                    <li key={subIndex}>
+                      <Link href={subItem.link} className="text-grey-40">
+                        {subItem.title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="wrapper flex flex-col items-center justify-center gap-y-[14px] py-5 text-center laptop:flex-row laptop:justify-between">
+        <ul className="flex flex-col gap-y-1.5 laptop:flex-row laptop:items-center laptop:divide-x laptop:divide-light-95">
+          {copyrightLinks.map((item, index) => (
+            <li
+              key={index}
+              className="first:pl-0 last:pr-0 laptop:px-5 desktop:px-6"
+            >
+              <Link
+                href={item.link}
+                className="text-sm text-grey-60 desktop:text-lg"
+              >
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <div className="hidden laptop:block">
+          <SocialNetworks />
+        </div>
+
+        <p className="text-sm text-grey-40 desktop:text-lg">
+          &copy; 2024 Klothink. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
