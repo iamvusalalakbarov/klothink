a5dcbf6d78f1997747261975e2208d03f28d58df
Vusal <iamvusalalakbarov@gmail.com>
2025-01-04 23:45:37 +0400

    Refactor Header component for improved layout and styling consistency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,67 +21,81 @@ const Header = () => {
   return (
     <>
       {/* Laptop, Desktop View */}
-      <header className="wrapper relative hidden items-center justify-between border-b border-light-95 bg-white py-[18px] laptop:flex">
-        <div className="flex items-center gap-x-2.5">
-          {navLinks.map((link, index) => (
+      <header className="hidden border-b border-light-95 bg-white laptop:block">
+        <div className="wrapper relative flex items-center justify-between bg-white py-[18px]">
+          <div className="flex items-center gap-x-2.5">
+            {navLinks.map((link, index) => (
+              <Link
+                key={index}
+                href={`/${link.slug}`}
+                className={clsx(
+                  'rounded-full px-5 py-3 text-sm font-medium desktop:px-6 desktop:py-[14px] desktop:text-lg',
+                  {
+                    'bg-light-97 text-grey-15': slug === link.slug,
+                    'border border-light-95 bg-white text-grey-40':
+                      slug !== link.slug,
+                  }
+                )}
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+
+          <Link
+            href="/"
+            className="absolute left-1/2 top-1/2 size-10 -translate-x-1/2 -translate-y-1/2 desktop:size-[60px]"
+          >
+            <Image
+              src="/images/logo.svg"
+              alt="Klothink Logo"
+              fill
+              unoptimized
+            />
+          </Link>
+
+          <div className="flex items-center gap-x-2.5 desktop:gap-x-[14px]">
+            <Link href="#" className="rounded-full bg-primary-50 p-3">
+              <ShoppingCartIcon className="size-5 text-grey-15 desktop:size-6" />
+            </Link>
             <Link
-              key={index}
-              href={`/${link.slug}`}
+              href="contact-support"
               className={clsx(
                 'rounded-full px-5 py-3 text-sm font-medium desktop:px-6 desktop:py-[14px] desktop:text-lg',
                 {
-                  'bg-light-97 text-grey-15': slug === link.slug,
+                  'bg-light-97 text-grey-15': slug === 'contact-support',
                   'border border-light-95 bg-white text-grey-40':
-                    slug !== link.slug,
+                    slug !== 'contact-support',
                 }
               )}
             >
-              {link.title}
+              Contact Support
             </Link>
-          ))}
-        </div>
-
-        <Link
-          href="/"
-          className="absolute left-1/2 top-1/2 size-10 -translate-x-1/2 -translate-y-1/2 desktop:size-[60px]"
-        >
-          <Image src="/images/logo.svg" alt="Klothink Logo" fill unoptimized />
-        </Link>
-
-        <div className="flex items-center gap-x-2.5 desktop:gap-x-[14px]">
-          <Link href="#" className="rounded-full bg-primary-50 p-3">
-            <ShoppingCartIcon className="size-5 text-grey-15 desktop:size-6" />
-          </Link>
-          <Link
-            href="contact-support"
-            className={clsx(
-              'rounded-full px-5 py-3 text-sm font-medium desktop:px-6 desktop:py-[14px] desktop:text-lg',
-              {
-                'bg-light-97 text-grey-15': slug === 'contact-support',
-                'border border-light-95 bg-white text-grey-40':
-                  slug !== 'contact-support',
-              }
-            )}
-          >
-            Contact Support
-          </Link>
+          </div>
         </div>
       </header>
 
       {/* Mobile View */}
-      <header className="wrapper flex items-center justify-between border-b border-light-95 bg-white py-5 laptop:hidden">
-        <Link href="/" className="relative size-9">
-          <Image src="/images/logo.svg" alt="Klothink Logo" fill unoptimized />
-        </Link>
-
-        <div className="flex items-center justify-center gap-x-2.5">
-          <Link href="#" className="rounded-full bg-primary-50 p-3">
-            <ShoppingCartIcon className="size-5 text-grey-15 desktop:size-6" />
+      <header className="border-b border-light-95 bg-white laptop:hidden">
+        <div className="wrapper flex items-center justify-between py-5">
+          <Link href="/" className="relative size-9">
+            <Image
+              src="/images/logo.svg"
+              alt="Klothink Logo"
+              fill
+              unoptimized
+            />
           </Link>
 
-          <button>
-            <Bars3BottomRightIcon className="size-8 text-grey-15" />
-          </button>
+          <div className="flex items-center justify-center gap-x-2.5">
+            <Link href="#" className="rounded-full bg-primary-50 p-3">
+              <ShoppingCartIcon className="size-5 text-grey-15 desktop:size-6" />
+            </Link>
+
+            <button>
+              <Bars3BottomRightIcon className="size-8 text-grey-15" />
+            </button>
+          </div>
         </div>
       </header>
     </>
